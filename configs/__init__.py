c7f31431e222cd882a57b9d0948fec61268e5de6
Spirin Egor <spirin.egor@mail.ru>
2020-06-26 17:58:26 +0300

    add code2seq config

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,5 +1,5 @@
 from .preprocessing_config import PreprocessingConfig
-from .code2seq_config import Code2SeqConfig
+from .code2seq_config import Code2SeqConfig, EncoderConfig
 
 
 def get_preprocessing_config_code2seq_params(dataset_name: str):

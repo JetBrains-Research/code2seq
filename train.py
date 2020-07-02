091a35316cdb48b5afd0d22c3ef0ed87a0f6feb5
Spirin Egor <spirin.egor@mail.ru>
2020-07-02 15:50:04 +0300

    set up logging interval

--- train.py
+++ train.py
@@ -27,16 +27,13 @@ def train(dataset_name: str, is_test: bool):
     model = Code2Seq(config, vocab)
 
     # define logger
-    wandb_logger = WandbLogger(project=f"code2seq-{dataset_name}", offline=is_test)
-    wandb_logger.watch(model, log="all", log_freq=config.log_every_epoch)
+    wandb_logger = WandbLogger(project=f"code2seq-{dataset_name}")
+    wandb_logger.watch(model)
     # define model checkpoint callback
     checkpoint_path = join(wandb.run.dir, "checkpoints")
     mkdir(checkpoint_path)
     model_checkpoint_callback = ModelCheckpoint(
-        filepath=join(checkpoint_path, "{epoch:02d}-{val_loss:.4f}"),
-        verbose=True,
-        period=config.save_every_epoch,
-        save_top_k=-1,
+        filepath=join(checkpoint_path, "{epoch:02d}-{val_loss:.4f}"), period=config.save_every_epoch, save_top_k=-1,
     )
     # define early stopping callback
     early_stopping_callback = EarlyStopping(patience=config.patience, verbose=True, mode="min")
@@ -45,6 +42,7 @@ def train(dataset_name: str, is_test: bool):
         gradient_clip_val=config.clip_norm,
         deterministic=True,
         check_val_every_n_epoch=config.val_every_epoch,
+        row_log_interval=config.log_every_epoch,
         logger=wandb_logger,
         checkpoint_callback=model_checkpoint_callback,
         early_stop_callback=early_stopping_callback,

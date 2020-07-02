6ec3e8a1ad07fe920c4af01cd5430cdc1a35f5cf
Spirin Egor <spirin.egor@mail.ru>
2020-07-02 20:05:55 +0300

    implement metrics

---
+++ tests/test_metrics.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+
+import torch
+
+from utils.metrics import SubtokenStatistic
+
+
+class TestSubtokenStatistic(TestCase):
+    def test_update(self):
+        st_stat = SubtokenStatistic(1, 2, 3)
+        st_stat_other = SubtokenStatistic(4, 5, 6)
+        st_stat.update(st_stat_other)
+
+        self.assertEquals(st_stat.true_positive, 5)
+        self.assertEquals(st_stat.false_positive, 7)
+        self.assertEquals(st_stat.false_negative, 9)
+
+    def test_calculate_metrics(self):
+        st_stat = SubtokenStatistic(3, 7, 2)
+        metrics = st_stat.calculate_metrics()
+        true_metrics = {"precision": 0.3, "recall": 0.6, "f1": 0.4}
+
+        self.assertDictEqual(metrics, true_metrics)
+
+    def test_calculate_statistic(self):
+        gt_subtokens = torch.tensor([[1, 1, 1, 0], [2, 2, 0, -1], [3, 3, -1, -1], [-1, -1, -1, -1]])
+        pred_subtokens = torch.tensor([[2, 4, 1, 0], [4, 5, 2, 0], [1, 6, 3, 0], [5, -1, -1, -1]])
+        skip = [-1, 0]
+
+        st_stat = SubtokenStatistic.calculate_statistic(gt_subtokens, pred_subtokens, skip)
+
+        self.assertEquals(st_stat.true_positive, 3)
+        self.assertEquals(st_stat.false_positive, 7)
+        self.assertEquals(st_stat.false_negative, 4)
---
+++ utils/metrics.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+from typing import Dict, List
+
+import torch
+
+
+@dataclass
+class SubtokenStatistic:
+    true_positive: int = 0
+    false_positive: int = 0
+    false_negative: int = 0
+
+    def update(self, other_statistic: "SubtokenStatistic"):
+        self.true_positive += other_statistic.true_positive
+        self.false_positive += other_statistic.false_positive
+        self.false_negative += other_statistic.false_negative
+
+    def calculate_metrics(self) -> Dict[str, int]:
+        precision, recall, f1 = 0, 0, 0
+        if self.true_positive + self.false_positive > 0:
+            precision = self.true_positive / (self.true_positive + self.false_positive)
+        if self.true_positive + self.false_negative > 0:
+            recall = self.true_positive / (self.true_positive + self.false_negative)
+        if precision + recall > 0:
+            f1 = 2 * precision * recall / (precision + recall)
+        return {"precision": precision, "recall": recall, "f1": f1}
+
+    @staticmethod
+    def calculate_statistic(
+        true_subtokens: torch.Tensor, predicted_subtokens: torch.Tensor, skip: List[int] = None
+    ) -> "SubtokenStatistic":
+        """Calculate subtoken statistic for ground truth and predicted batches of labels.
+
+        :param true_subtokens: [true seq length; batch size] ground truth labels
+        :param predicted_subtokens: [pred seq length; batch size] predicted labels
+        :param skip: list of subtokens ids that should be ignored
+        :return: dataclass with calculated statistic
+        """
+        if true_subtokens.shape[1] != predicted_subtokens.shape[1]:
+            raise ValueError(
+                f"unequal batch sizes for ground truth ({true_subtokens.shape[1]}) subtokens"
+                f"and predicted ({predicted_subtokens.shape[1]})"
+            )
+        if skip is None:
+            skip = []
+        subtoken_statistic = SubtokenStatistic()
+        batch_size = true_subtokens.shape[1]
+        for batch_idx in range(batch_size):
+            gt_seq = [st for st in true_subtokens[:, batch_idx] if st not in skip]
+            pred_seq = [st for st in predicted_subtokens[:, batch_idx] if st not in skip]
+            for pred_subtoken in pred_seq:
+                if pred_subtoken in gt_seq:
+                    subtoken_statistic.true_positive += 1
+                else:
+                    subtoken_statistic.false_positive += 1
+            for gt_subtoken in gt_seq:
+                if gt_subtoken not in pred_seq:
+                    subtoken_statistic.false_negative += 1
+        return subtoken_statistic

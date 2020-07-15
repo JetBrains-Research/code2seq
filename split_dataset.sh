0336665a656e484a4293b8f5141e49b03f924695
maximzubkov <zubkov.md@phystech.edu>
2020-07-15 11:42:46 +0300

    Add command-line arguments for split_dataset.sh

--- split_dataset.sh
+++ split_dataset.sh
@@ -1,14 +1,83 @@
 #!/bin/bash
 # Script - split data between train and test
 # Default values
-train=70
-val=10
+train=60
+val=20
 test=20
 original_dataset_path="./data/poj_104"
 split_dataset_path="./data/poj_104_split"
-dir_train=${split_dataset_path}"/train"
-dir_validation=${split_dataset_path}"/validation"
-dir_test=${split_dataset_path}"/test"
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "options:"
+      echo "-h, --help                show brief help"
+      echo "-i, --input-dir=DIR       specify a directory where dataset is located"
+      echo "-o, --output-dir=DIR      specify a directory to store output in"
+      echo "--train=VAL               specify a percentage of dataset used as train set"
+      echo "--test=VAL                specify a percentage of dataset used as test set"
+      echo "--val=VAL                 specify a percentage of dataset used as validation set"
+      exit 0
+      ;;
+    -i|--input-dir*)
+      shift
+      if test $# -gt 0; then
+        original_dataset_path=$1
+      else
+        echo "no input dir specified, using default: ./data/poj_104"
+        exit 1
+      fi
+      shift
+      ;;
+    -o|--output-dir*)
+      shift
+      if test $# -gt 0; then
+        split_dataset_path=$1
+      else
+        echo "no output dir specified, using default: ./data/poj_104_split"
+        exit 1
+      fi
+      shift
+      ;;
+    --train*)
+      shift
+      if test $# -gt 0; then
+        train=$1
+      else
+        echo "no train specified, using default: 60 %"
+        exit 1
+      fi
+      shift
+      ;;
+    --test*)
+      shift
+      if test $# -gt 0; then
+        test=$1
+      else
+        echo "no test specified, using default: 20 %"
+        exit 1
+      fi
+      shift
+      ;;
+    --val*)
+      shift
+      if test $# -gt 0; then
+        val=$1
+      else
+        echo "no val specified, using default: 20 %"
+        exit 1
+      fi
+      shift
+      ;;
+    *)
+      echo "something went wrong"
+      exit 1
+  esac
+done
+
+dir_train="${split_dataset_path}/train"
+dir_validation="${split_dataset_path}/validation"
+dir_test="${split_dataset_path}/test"
 
 echo "Train $train % "
 echo "Val $val %"
@@ -19,7 +88,7 @@ echo "Val dataset path = ${dir_validation}"
 echo "Test dataset path = ${dir_test}"
 
 echo ""
-echo "Removing all data inside "${split_dataset_path}
+echo "Removing all data inside ${split_dataset_path}"
 rm -rf $split_dataset_path
 mkdir $split_dataset_path
 

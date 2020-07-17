dbadb2183d22b114d388fbfce7586a2a673a9c8f
maximzubkov <zubkov.md@phystech.edu>
2020-07-17 18:33:06 +0300

    Global refactor of stminer parser

--- data_preprocessing/preprocess_astminer_code2vec_data.py
+++ data_preprocessing/preprocess_astminer_code2vec_data.py
@@ -1,120 +1,52 @@
 from argparse import ArgumentParser
 from collections import Counter
-from os import path
+from os import path, remove
 from typing import Tuple, List, Dict
 from multiprocessing import cpu_count
-
 from tqdm import tqdm
+
 import numpy
-import pickle
 
-from configs import get_preprocessing_config_code2seq_params, PreprocessingConfig
-from dataset import Vocabulary
-from utils.common import UNK, count_lines_in_file
+from configs import get_preprocessing_config_code2seq_params
 
-from utils.preprocessing import convert_holdout, DATA_FOLDER, vocab_from_counters
+from data_preprocessing.preprocess_code2seq_data import preprocess, DATA_FOLDER
+from utils.common import FROM_TOKEN, PATH_TYPES, TO_TOKEN, count_lines_in_file, create_folder, SOS, EOS, PAD, UNK
 
 
-def _get_id2value_from_csv(data_path: str) -> Dict[int, str]:
-    data = numpy.genfromtxt(data_path, delimiter=",", dtype=(str, str))[1:]
-    return {int(index): values for index, values in data}
+def _get_id2value_from_csv(data_path: str) -> Dict[str, str]:
+    return dict(numpy.genfromtxt(data_path, delimiter=",", dtype=(str, str))[1:])
 
 
-def preprocess_csv(data_path: str, holdout_name: str):
+def preprocess_csv(data_folder: str, dataset_name: str, holdout_name: str):
     """
     Preprocessing for files tokens.csv, paths.csv, node_types.csv
     """
+    data_path = path.join(data_folder, dataset_name)
     token_data_path = path.join(data_path, f"tokens.{holdout_name}.csv")
     type_data_path = path.join(data_path, f"node_types.{holdout_name}.csv")
     paths_data_path = path.join(data_path, f"paths.{holdout_name}.csv")
+    path_contexts_path = path.join(data_path, f"path_contexts.{holdout_name}.csv")
+    poj_c2s_path = path.join(data_path, f"{dataset_name}.{holdout_name}.c2s")
 
-    paths = _get_id2value_from_csv(paths_data_path)
-    paths = {index: [int(n) for n in nodes.split()] for index, nodes in paths.items()}
+    id_to_paths = _get_id2value_from_csv(paths_data_path)
+    id_to_paths = {index: [n for n in nodes.split()] for index, nodes in id_to_paths.items()}
 
-    node_types = _get_id2value_from_csv(type_data_path)
-    node_types = {index: node_type.rsplit(" ", maxsplit=1)[0] for index, node_type in node_types.items()}
+    id_to_node_types = _get_id2value_from_csv(type_data_path)
+    id_to_node_types = {index: node_type.rsplit(" ", maxsplit=1)[0] for index, node_type in id_to_node_types.items()}
 
-    tokens = _get_id2value_from_csv(token_data_path)
-    tokens = {index: token_seq.split("|") for index, token_seq in tokens.items()}
+    id_to_tokens = _get_id2value_from_csv(token_data_path)
 
-    return tokens, node_types, paths
-
-
-def collect_vocabulary(
-    config: PreprocessingConfig,
-    tokens: Dict[int, List[int]],
-    node_types: Dict[int, List[int]],
-    paths: Dict[int, List[int]],
-) -> Vocabulary:
-    train_contexts_path = path.join(DATA_FOLDER, config.dataset_name, "path_contexts.train.csv")
-    target_counter = Counter()
-    token_counter = Counter()
-    type_counter = Counter()
-
-    with open(train_contexts_path, "r") as train_contexts_file:
-        for line in tqdm(train_contexts_file, total=count_lines_in_file(train_contexts_path)):
+    remove(poj_c2s_path)
+    with open(path_contexts_path, "r") as path_contexts_file, open(poj_c2s_path, "a+") as output:
+        for line in tqdm(path_contexts_file, total=count_lines_in_file(path_contexts_path)):
             label, *path_contexts = line.split()
-            target_counter.update([label])
-            cur_tokens = []
-            cur_types = []
-            for pc in path_contexts:
-                from_token_id, path_id, to_token_id = (int(_) for _ in pc.split(","))
-                # Extracting paths
-                cur_types += [node_types[_id] for _id in paths[path_id]]
-
-                cur_tokens += tokens[from_token_id] + tokens[to_token_id]
-            token_counter.update(cur_tokens)
-            type_counter.update(cur_types)
-
-    return vocab_from_counters(config, token_counter, target_counter, type_counter)
-
-
-def convert_path_context_to_ids(
-    path_context: str,
-    vocab: Vocabulary,
-    paths: Dict[int, List[int]],
-    node_types: Dict[int, List[int]],
-    tokens: Dict[int, List[int]],
-) -> Tuple[List[int], List[int], List[int]]:
-    from_token_id, path_types_id, to_token_id = (int(_) for _ in path_context.split(","))
-    nodes = paths[path_types_id]
-    type_unk = vocab.type_to_id[UNK]
-
-    from_tokens, to_tokens = tokens[from_token_id], tokens[to_token_id]
-    token_unk = vocab.token_to_id[UNK]
-
-    return (
-        [vocab.token_to_id.get(_t, token_unk) for _t in from_tokens],
-        [vocab.type_to_id.get(node_types[_n], type_unk) for _n in nodes],
-        [vocab.token_to_id.get(_t, token_unk) for _t in to_tokens],
-    )
-
-
-def preprocess(config: PreprocessingConfig, n_jobs: int):
-    # Collect vocabulary from train holdout if needed
-    data_path = path.join(DATA_FOLDER, config.dataset_name)
-    vocab_path = path.join(data_path, "vocabulary.pkl")
-
-    if path.exists(vocab_path):
-        vocab = Vocabulary.load(vocab_path)
-    else:
-        tokens, node_types, paths = preprocess_csv(data_path, "train")
-        vocab = collect_vocabulary(config, tokens=tokens, paths=paths, node_types=node_types)
-        vocab.dump(vocab_path)
-
-    for holdout_name in "train", "test", "val":
-        tokens, node_types, paths = preprocess_csv(data_path, holdout_name)
-        convert_holdout(
-            data_path,
-            holdout_name,
-            vocab,
-            config,
-            n_jobs,
-            convert_path_context_to_ids,
-            paths=paths,
-            tokens=tokens,
-            node_types=node_types,
-        )
+            parsed_line = [label]
+            for path_context in path_contexts:
+                from_token_id, path_types_id, to_token_id = path_context.split(",")
+                from_token, to_token = id_to_tokens[from_token_id], id_to_tokens[to_token_id]
+                nodes = [id_to_node_types[p_] for p_ in id_to_paths[path_types_id]]
+                parsed_line.append(",".join([from_token, "|".join(nodes), to_token]))
+            output.write(" ".join(parsed_line))
 
 
 if __name__ == "__main__":
@@ -122,7 +54,8 @@ if __name__ == "__main__":
     arg_parser.add_argument("data", type=str)
     arg_parser.add_argument("--n-jobs", type=int, default=None)
     args = arg_parser.parse_args()
+    data_path = path.join(DATA_FOLDER, args.data)
+    for holdout_name in "train", "test", "val":
+        preprocess_csv(DATA_FOLDER, args.data, holdout_name)
 
-    preprocess(
-        get_preprocessing_config_code2seq_params(args.data), args.n_jobs or cpu_count(),
-    )
+    preprocess(get_preprocessing_config_code2seq_params(args.data), True, args.n_jobs or cpu_count())
--- data_preprocessing/preprocess_code2seq_data.py
+++ data_preprocessing/preprocess_code2seq_data.py
@@ -1,16 +1,30 @@
 import pickle
 from argparse import ArgumentParser
 from collections import Counter
-from multiprocessing import cpu_count
+from math import ceil
+from multiprocessing import Pool, cpu_count
 from os import path
-from typing import Tuple, List
+from typing import Tuple, List, Generator
 
 from tqdm import tqdm
 
 from configs import get_preprocessing_config_code2seq_params, PreprocessingConfig
-from dataset import Vocabulary
-from utils.common import UNK, count_lines_in_file
-from utils.preprocessing import convert_holdout, DATA_FOLDER, vocab_from_counters
+from dataset import Vocabulary, BufferedPathContext, ConvertParameters
+from utils.common import SOS, EOS, PAD, UNK, count_lines_in_file, create_folder, FROM_TOKEN, TO_TOKEN, PATH_TYPES
+
+DATA_FOLDER = "data"
+
+
+def _vocab_from_counters(
+    config: PreprocessingConfig, token_counter: Counter, target_counter: Counter, type_counter: Counter
+) -> Vocabulary:
+    vocab = Vocabulary()
+    vocab.add_from_counter(
+        "token_to_id", token_counter, config.subtoken_vocab_max_size, [SOS, EOS, PAD, UNK],
+    )
+    vocab.add_from_counter("label_to_id", target_counter, config.target_vocab_max_size, [SOS, EOS, PAD, UNK])
+    vocab.add_from_counter("type_to_id", type_counter, -1, [SOS, EOS, PAD, UNK])
+    return vocab
 
 
 def collect_vocabulary(config: PreprocessingConfig) -> Vocabulary:
@@ -30,18 +44,18 @@ def collect_vocabulary(config: PreprocessingConfig) -> Vocabulary:
                 cur_types += path_types.split("|")
             token_counter.update(cur_tokens)
             type_counter.update(cur_types)
-    return vocab_from_counters(config, token_counter, target_counter, type_counter)
+    return _vocab_from_counters(config, token_counter, target_counter, type_counter)
 
 
 def convert_vocabulary(config: PreprocessingConfig) -> Vocabulary:
-    with open(path.join(DATA_FOLDER, config.dataset_name, f"{config.dataset_name}.dict.c2s"), "rb",) as dict_file:
+    with open(path.join(DATA_FOLDER, config.dataset_name, f"{config.dataset_name}.dict.c2s"), "rb") as dict_file:
         subtoken_to_count = Counter(pickle.load(dict_file))
         node_to_count = Counter(pickle.load(dict_file))
         target_to_count = Counter(pickle.load(dict_file))
-    return vocab_from_counters(config, subtoken_to_count, target_to_count, node_to_count)
+    return _vocab_from_counters(config, subtoken_to_count, target_to_count, node_to_count)
 
 
-def convert_path_context_to_ids(path_context: str, vocab: Vocabulary) -> Tuple[List[int], List[int], List[int]]:
+def _convert_path_context_to_ids(path_context: str, vocab: Vocabulary) -> Tuple[List[int], List[int], List[int]]:
     from_token, path_types, to_token = path_context.split(",")
     token_unk = vocab.token_to_id[UNK]
     type_unk = vocab.type_to_id[UNK]
@@ -52,6 +66,54 @@ def convert_path_context_to_ids(path_context: str, vocab: Vocabulary) -> Tuple[L
     )
 
 
+def _convert_raw_buffer(convert_args: Tuple[List[str], PreprocessingConfig, Vocabulary, str]):
+    lines, config, vocab, output_path = convert_args
+    labels, from_tokens, path_types, to_tokens = [], [], [], []
+    for line in lines:
+        label, *path_contexts = line.split()
+        labels.append([vocab.label_to_id.get(_l, vocab.label_to_id[UNK]) for _l in label.split("|")])
+        converted_context = [_convert_path_context_to_ids(pc, vocab) for pc in path_contexts]
+        from_tokens.append([cc[0] for cc in converted_context])
+        path_types.append([cc[1] for cc in converted_context])
+        to_tokens.append([cc[2] for cc in converted_context])
+
+    BufferedPathContext.create_from_lists(
+        (labels, ConvertParameters(config.max_target_parts, config.wrap_target, vocab.label_to_id)),
+        {
+            FROM_TOKEN: (from_tokens, ConvertParameters(config.max_name_parts, config.wrap_name, vocab.token_to_id),),
+            PATH_TYPES: (path_types, ConvertParameters(config.max_path_length, config.wrap_path, vocab.type_to_id)),
+            TO_TOKEN: (to_tokens, ConvertParameters(config.max_name_parts, config.wrap_name, vocab.token_to_id)),
+        },
+    ).dump(output_path)
+
+
+def _read_file_by_batch(filepath: str, batch_size: int) -> Generator[List[str], None, None]:
+    with open(filepath, "r") as file:
+        lines = []
+        for line in file:
+            lines.append(line.strip())
+            if len(lines) == batch_size:
+                yield lines
+                lines = []
+    yield lines
+
+
+def convert_holdout(holdout_name: str, vocab: Vocabulary, config: PreprocessingConfig, n_jobs: int):
+    holdout_data_path = path.join(DATA_FOLDER, config.dataset_name, f"{config.dataset_name}.{holdout_name}.c2s")
+    holdout_output_folder = path.join(DATA_FOLDER, config.dataset_name, holdout_name)
+    create_folder(holdout_output_folder)
+    n_buffers = ceil(count_lines_in_file(holdout_data_path) / config.buffer_size)
+    with Pool(n_jobs) as pool:
+        results = pool.imap(
+            _convert_raw_buffer,
+            (
+                (lines, config, vocab, path.join(holdout_output_folder, f"buffered_paths_{pos}.pkl"))
+                for pos, lines in enumerate(_read_file_by_batch(holdout_data_path, config.buffer_size))
+            ),
+        )
+        _ = [_ for _ in tqdm(results, total=n_buffers)]
+
+
 def preprocess(config: PreprocessingConfig, is_vocab_collected: bool, n_jobs: int):
     # Collect vocabulary from train holdout if needed
     vocab_path = path.join(DATA_FOLDER, config.dataset_name, "vocabulary.pkl")
@@ -60,9 +122,9 @@ def preprocess(config: PreprocessingConfig, is_vocab_collected: bool, n_jobs: in
     else:
         vocab = collect_vocabulary(config) if is_vocab_collected else convert_vocabulary(config)
         vocab.dump(vocab_path)
-    data_path = path.join(DATA_FOLDER, config.dataset_name)
-    for holdout_name in "train", "test", "val":
-        convert_holdout(data_path, holdout_name, vocab, config, n_jobs, convert_path_context_to_ids)
+    convert_holdout("train", vocab, config, n_jobs)
+    convert_holdout("val", vocab, config, n_jobs)
+    convert_holdout("test", vocab, config, n_jobs)
 
 
 if __name__ == "__main__":

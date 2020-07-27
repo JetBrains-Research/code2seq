cd759552e33abe71e83b145b0b9286a5241c974c
maximzubkov <zubkov.md@phystech.edu>
2020-07-27 16:11:12 +0300

    Fix wrapping labels and add wrapping to other counters

--- data_preprocessing/preprocess_code2seq_data.py
+++ data_preprocessing/preprocess_code2seq_data.py
@@ -22,12 +22,12 @@ def _vocab_from_counters(
     config: PreprocessingConfig, token_counter: Counter, target_counter: Counter, type_counter: Counter
 ) -> Vocabulary:
     vocab = Vocabulary()
-    vocab.add_from_counter("token_to_id", token_counter, config.subtoken_vocab_max_size, [SOS, EOS, PAD, UNK])
-    vocab.add_from_counter("type_to_id", type_counter, -1, [SOS, EOS, PAD, UNK])
-    if config.wrap_target:
-        vocab.add_from_counter("label_to_id", target_counter, config.target_vocab_max_size, [SOS, EOS, PAD, UNK])
-    else:
-        vocab.add_from_counter("label_to_id", type_counter, -1, [PAD, UNK])
+    names_additional_tokens = [SOS, EOS, PAD, UNK] if config.wrap_name else [PAD, UNK]
+    vocab.add_from_counter("token_to_id", token_counter, config.subtoken_vocab_max_size, names_additional_tokens)
+    target_additional_tokens = [SOS, EOS, PAD, UNK] if config.wrap_target else [PAD, UNK]
+    vocab.add_from_counter("label_to_id", target_counter, config.target_vocab_max_size, target_additional_tokens)
+    paths_additional_tokens = [SOS, EOS, PAD, UNK] if config.wrap_path else [PAD, UNK]
+    vocab.add_from_counter("type_to_id", type_counter, -1, paths_additional_tokens)
     return vocab
 
 

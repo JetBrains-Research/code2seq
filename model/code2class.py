0f87380246757878c2c6eb4fe7a9ae8f1a77a596
maximzubkov <zubkov.md@phystech.edu>
2020-07-28 15:44:58 +0300

    Remove classification metrics

--- model/code2class.py
+++ model/code2class.py
@@ -35,49 +35,21 @@ class Code2Class(BaseCodeModel):
 
     def _general_epoch_end(self, outputs: List[Dict], loss_key: str, group: str) -> Dict:
         logs = {f"{group}/loss": torch.stack([out[loss_key] for out in outputs]).mean()}
-        # Computing accuracy throughout the epoch
-        correct = sum([out[f"{group}/correct"] for out in outputs])
-        total = sum([out[f"{group}/total"] for out in outputs])
-        logs.update({f"{group}/accuracy": correct / total})
-
-        progress_bar = {k: v for k, v in logs.items() if k in [f"{group}/loss", f"{group}/accuracy"]}
-
+        progress_bar = {k: v for k, v in logs.items() if k in [f"{group}/loss"]}
         return {"val_loss": logs[f"{group}/loss"], "log": logs, "progress_bar": progress_bar}
-
-    @staticmethod
-    def _count_correct(logits: torch.Tensor, labels: torch.Tensor) -> int:
-        """ Calculate correctly predicted labels
-        :param logits: [batch size; num classes]
-        :param labels: [batch size]
-        :return: [1]
-        """
-        indexes = logits.argmax(-1).detach()
-        labels = labels.detach()
-        return (indexes == labels).sum().item()
 
     def training_step(self, batch: PathContextBatch, batch_idx: int) -> Dict:
         # [batch size; num_classes]
         logits = self(batch.context, batch.contexts_per_label)
         loss = F.cross_entropy(logits, batch.labels.squeeze(0))
-        with torch.no_grad():
-            correct, total = self._count_correct(logits, batch.labels.squeeze(0)), logits.shape[0]
-            log = {
-                "train/loss": loss,
-                "train/accuracy": correct / total,
-                "train/total": total,
-                "train/correct": correct,
-            }
-        progress_bar = {"train/accuracy": log["train/accuracy"]}
-        return {"loss": loss, "log": log, "progress_bar": progress_bar, "train/total": total, "train/correct": correct}
+        log = {"train/loss": loss}
+        return {"loss": loss, "log": log}
 
     def validation_step(self, batch: PathContextBatch, batch_idx: int) -> Dict:
         # [batch size; num_classes]
         logits = self(batch.context, batch.contexts_per_label)
         loss = F.cross_entropy(logits, batch.labels.squeeze(0))
-        with torch.no_grad():
-            correct, total = self._count_correct(logits, batch.labels.squeeze(0)), logits.shape[0]
-
-        return {"val_loss": loss, "val/total": total, "val/correct": correct}
+        return {"val_loss": loss}
 
     def test_step(self, batch: PathContextBatch, batch_idx: int) -> Dict:
         result = self.validation_step(batch, batch_idx)

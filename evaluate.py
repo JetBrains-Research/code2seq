abf43d14708365bf880e0c90988e120e23513337
Egor Spirin <spirin.egor@mail.ru>
2020-08-13 14:50:40 +0300

    use only custom dataloader for evaluate script

--- evaluate.py
+++ evaluate.py
@@ -10,29 +10,21 @@ from dataset import create_dataloader
 from model import Code2Seq
 
 
-DATA_FOLDER = "data"
 SEED = 7
 
 
 def evaluate(checkpoint: str, data: str = None, batch_size: int = None):
     seed_everything(SEED)
     model = Code2Seq.load_from_checkpoint(checkpoint_path=checkpoint)
-    model.hyperparams.batch_size = batch_size or model.hyperparams.batch_size
+    batch_size = batch_size or model.hyperparams.test_batch_size
+    data = data or model.hyperparams.test_data_path
     gpu = 1 if torch.cuda.is_available() else None
+    data_loader, n_samples = create_dataloader(
+        data, model.hyperparams.max_context, False, False, batch_size, cpu_count(),
+    )
+    print(f"approximate number of steps for test is {ceil(n_samples / batch_size)}")
     trainer = Trainer(gpus=gpu)
-    if data is not None:
-        data_loader, n_samples = create_dataloader(
-            join(DATA_FOLDER, data),
-            model.hyperparams.max_context,
-            False,
-            False,
-            model.hyperparams.test_batch_size,
-            cpu_count(),
-        )
-        print(f"approximate number of steps for test is {ceil(n_samples / model.config.test_batch_size)}")
-        trainer.test(model, test_dataloaders=data_loader)
-    else:
-        trainer.test(model)
+    trainer.test(model, test_dataloaders=data_loader)
 
 
 if __name__ == "__main__":

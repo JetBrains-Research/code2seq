227851c13aa2f626fc9c653d1ce8ccd1c7d4a91f
Egor Spirin <spirin.egor@mail.ru>
2020-10-30 16:39:58 +0300

    simplify creating batch from list of samples

--- dataset/data_classes.py
+++ dataset/data_classes.py
@@ -16,20 +16,16 @@ class PathContextBatch:
     def __init__(self, samples: List[PathContextSample]):
         self.contexts_per_label = [_s.n_contexts for _s in samples]
 
-        tensor_labels = [torch.tensor(_s.label, dtype=torch.long) for _s in samples]
-        self.labels = torch.cat(tensor_labels, dim=-1)
+        torch_labels = numpy.hstack([_s.label for _s in samples])
+        self.labels = torch.from_numpy(torch_labels)
 
         self.contexts = {}
-        for sample in samples:
-            for key, value in sample.contexts.items():
-                if key not in self.contexts:
-                    self.contexts[key] = []
-                self.contexts[key].append(torch.tensor(value, dtype=torch.long))
-        for key, tensor_list in self.contexts.items():
-            self.contexts[key] = torch.cat(tensor_list, dim=-1)
+        for key in samples[0].contexts:
+            key_union = numpy.hstack([_s.contexts[key] for _s in samples])
+            self.contexts[key] = torch.from_numpy(key_union)
 
     def __len__(self) -> int:
-        return self.labels.shape[1]
+        return len(self.contexts_per_label)
 
     def pin_memory(self) -> "PathContextBatch":
         self.labels = self.labels.pin_memory()

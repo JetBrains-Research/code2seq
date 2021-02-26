53222b0998239d4b85056381b08f8a4daed57e4d
Egor Spirin <spirin.egor@mail.ru>
2021-02-26 17:40:55 +0300

    Create setup script

--- code2seq/utils/common.py
+++ code2seq/utils/common.py
@@ -36,7 +36,7 @@ def print_config(config: DictConfig):
 
 def filter_warnings():
     # "The dataloader does not have many workers which may be a bottleneck."
-    filterwarnings("ignore", category=UserWarning, module="pytorch_lightning.utilities.distributed", lineno=45)
+    filterwarnings("ignore", category=UserWarning, module="pytorch_lightning.utilities.distributed", lineno=50)
     # "Please also save or load the state of the optimizer when saving or loading the scheduler."
     filterwarnings("ignore", category=UserWarning, module="torch.optim.lr_scheduler", lineno=216)  # save
     filterwarnings("ignore", category=UserWarning, module="torch.optim.lr_scheduler", lineno=234)  # load

f8ffa007dd1d9bfcabdd079a85b798b4e4e5f496
malodetz <malo.detz@yandex.ru>
2022-07-15 11:02:13 +0300

    Added comment label processing

--- code2seq/data/comment_path_context_dataset.py
+++ code2seq/data/comment_path_context_dataset.py
@@ -1,19 +1,23 @@
 from typing import Dict, List, Optional
 
+from omegaconf import DictConfig
+from transformers import RobertaTokenizerFast
+
 from code2seq.data.path_context_dataset import PathContextDataset
 from code2seq.data.vocabulary import Vocabulary
 
 
 class CommentPathContextDataset(PathContextDataset):
+    def __init__(self, data_file: str, config: DictConfig, vocabulary: Vocabulary, random_context: bool):
+        super().__init__(data_file, config, vocabulary, random_context)
 
     @staticmethod
     def tokenize_label(raw_label: str, vocab: Dict[str, int], max_parts: Optional[int]) -> List[int]:
-        sublabels = raw_label.split(PathContextDataset._separator)
-        max_parts = max_parts or len(sublabels)
-        label_unk = vocab[Vocabulary.UNK]
+        tokenizer = RobertaTokenizerFast.from_pretrained("microsoft/codebert-base")
 
-        label = [vocab[Vocabulary.SOS]] + [vocab.get(st, label_unk) for st in sublabels[:max_parts]]
-        if len(sublabels) < max_parts:
-            label.append(vocab[Vocabulary.EOS])
-            label += [vocab[Vocabulary.PAD]] * (max_parts + 1 - len(label))
-        return label
+        label_with_spaces = ' '.join(raw_label.split(PathContextDataset._separator))
+        label_tokens = [tokenizer.bos_token] + tokenizer.tokenize(label_with_spaces)[:max_parts - 2] + [
+            tokenizer.eos_token]
+        label_tokens += [tokenizer.pad_token] * (max_parts - len(label_tokens))
+
+        return tokenizer.convert_tokens_to_ids(label_tokens)

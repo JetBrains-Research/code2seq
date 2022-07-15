c4a203b4ebb7a821a40b5ccd26f95f3d8f28196a
malodetz <malo.detz@yandex.ru>
2022-07-15 12:58:07 +0300

    Fix black

--- code2seq/data/comment_path_context_data_module.py
+++ code2seq/data/comment_path_context_data_module.py
@@ -3,7 +3,6 @@ from code2seq.data.path_context_data_module import PathContextDataModule
 
 
 class CommentPathContextDataModule(PathContextDataModule):
-
     def _create_dataset(self, holdout_file: str, random_context: bool) -> CommentPathContextDataset:
         if self._vocabulary is None:
             raise RuntimeError(f"Setup vocabulary before creating data loaders")
--- code2seq/model/comment_code2seq.py
+++ code2seq/model/comment_code2seq.py
@@ -5,7 +5,6 @@ from commode_utils.losses import SequenceCrossEntropyLoss
 from commode_utils.metrics import SequentialF1Score, ClassificationMetrics
 from commode_utils.modules import LSTMDecoderStep, Decoder
 from omegaconf import DictConfig
-from pytorch_lightning import LightningModule
 from pytorch_lightning.utilities.types import EPOCH_OUTPUT
 from torchmetrics import MetricCollection, Metric
 from transformers import RobertaTokenizerFast
@@ -17,11 +16,11 @@ from code2seq.model import Code2Seq
 
 class CommentCode2Seq(Code2Seq):
     def __init__(
-            self,
-            model_config: DictConfig,
-            optimizer_config: DictConfig,
-            vocabulary: Vocabulary,
-            teacher_forcing: float = 0.0,
+        self,
+        model_config: DictConfig,
+        optimizer_config: DictConfig,
+        vocabulary: Vocabulary,
+        teacher_forcing: float = 0.0,
     ):
         super().__init__(model_config, optimizer_config, vocabulary, teacher_forcing)
 
@@ -33,7 +32,7 @@ class CommentCode2Seq(Code2Seq):
             f"{holdout}_f1": SequentialF1Score(pad_idx=self.__pad_idx, eos_idx=eos_idx, ignore_idx=ignore_idx)
             for holdout in ["train", "val", "test"]
         }
-        #TODO add chrf back
+        # TODO add chrf back
         self.__metrics = MetricCollection(metrics)
 
         self._encoder = self._get_encoder(model_config)
